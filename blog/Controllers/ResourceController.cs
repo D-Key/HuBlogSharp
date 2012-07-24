de9b41fa0c67762d6cf780a9b3d8f63735d9d61a
unknown <dmitriy@dmitriy.hq.fogcreek.com>
2012-07-24 18:25:51 -0400

    initial commit

---
+++ blog/Controllers/ResourceController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace blog.Controllers
+{
+    public class ResourceController : Controller
+    {
+        private const string ResourceRoot = "~/Content";
+
+        public static void MapRoutes(RouteCollection routes)
+        {
+            var cssDefaults = new { controller = "Resource", action = "Css", id = UrlParameter.Optional };
+            var jsDefaults  = new { controller = "Resource", action = "Js", id = UrlParameter.Optional };
+
+            routes.IgnoreRoute("*.js|css");
+            routes.MapRoute("Resource Css", "{id}.css", cssDefaults, new { id = ".*" });
+            routes.MapRoute("Resource Js", "{id}.js", jsDefaults);
+        }
+
+        public ActionResult Css(string id = null)
+        {
+            return ServeCombineableResource(id, "css", "text/css");
+        }
+
+        public ActionResult Js(string id = null)
+        {
+            return ServeCombineableResource(id, "js", "text/javascript");
+        }
+
+        public ActionResult ServeCombineableResource(string id, string extension, string mime)
+        {
+            var rootPath = Server.MapPath(ResourceRoot);
+            var filePath = System.IO.Path.Combine(rootPath, id + "." + extension);
+            if (System.IO.File.Exists(filePath))
+                return File(filePath, mime);
+
+            switch (id)
+            {
+                case "all":
+                    if (System.IO.Directory.Exists(rootPath))
+                    {
+                        // get all files, pack in one and serve
+                        foreach (var fileName in System.IO.Directory.GetFiles(rootPath, "*." + extension))
+                        {
+                            throw new NotImplementedException();
+                        }
+                    }
+                    break;
+            }
+
+            return HttpNotFound();
+        }
+    }
+}
---
+++ blog/Global.asax.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace blog
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            // Controller-specific rules in order of being checked
+            Controllers.ResourceController.MapRoutes(routes);
+            // Default route should be defined and applied last
+            routes.MapRoute("Default", "{controller}/{action}/{id}", new { controller = "Blog", action = "Index", id = UrlParameter.Optional });
+        }
+
+        protected void Application_Start()
+        {
+            // Uncomment RegisterAllAreas() call if you use areas (Project dropdown menu > Add > Area)
+            // AreaRegistration.RegisterAllAreas();
+            RegisterGlobalFilters(GlobalFilters.Filters);
+            RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
