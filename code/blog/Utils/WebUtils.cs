49ff9814242c811b59ff61e6a930104c349f8801
DeeKey <dkomarov@gmail.com>
2012-07-25 15:06:22 -0400

    LoadFromUrl utility

---
+++ code/blog/Utils/WebUtils.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace blog.Utils
+{
+    public static class WebUtils
+    {
+        public static string LoadFromUrl(string url)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (StreamReader sr = new StreamReader(response.GetResponseStream(), Encoding.UTF8))
+            {
+                return sr.ReadToEnd();
+            }
+        }
+    }
+}
